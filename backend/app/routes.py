c818ce8615955c37bed6547439330aa6674393d9
justflud <stasikkyropatkin@gmail.com>
2026-03-09 12:42:19 +0530

    change backend

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,26 @@
 from flask import Blueprint, jsonify
+from .models import Ingredient
+
 main = Blueprint("main", __name__)
 
+
 @main.route("/")
-def home ():
+def home():
     return jsonify({"message": "Backend is working!"})
+
+
+@main.route("/ingredients", methods=["GET"])
+def get_ingredients():
+    ingredients = Ingredient.query.all()
+
+    result = []
+
+    for ingredient in ingredients:
+        result.append({
+            "id": ingredient.id,
+            "name": ingredient.name,
+            "category": ingredient.category,
+            "calories_per_100g": ingredient.calories_per_100g
+        })
+
+    return jsonify(result)
