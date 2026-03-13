07f27c98f49e45e7e499c46f6f63f63b3ebcf594
justflud <stasikkyropatkin@gmail.com>
2026-03-13 23:55:44 +0530

    add extensions and registration

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost:5432/med_diet_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    
