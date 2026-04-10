77e9b65ed4fcf71ce46ff144f39a0a8b32932520
justflud <stasikkyropatkin@gmail.com>
2026-04-11 01:51:18 +0530

    add refresh token and change password

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from app.extensions import db, bcrypt, jwt
+from app.extensions import db, bcrypt, jwt, jwt_blocklist
 
 
 def create_app():
@@ -10,6 +10,10 @@ def create_app():
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_revoked(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in jwt_blocklist
 
     from app import models
 
